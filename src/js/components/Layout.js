3eb328bf2a39f516c927ca10883095030afc8a41
Maciej Sularz <sularz.maciej@gmail.com>
2016-10-22 00:39:12 +0200

    Created the barebones of the app

---
+++ src/js/components/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+// by exporting this class we can require it in client.js
+// https://developer.mozilla.org/en/docs/web/javascript/reference/statements/export#Using_the_default_export
+export default class Layout extends React.Component {
+	render(){
+		return(
+			<div>datainvest.pl</div>
+		);
+	}
+}
