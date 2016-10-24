2118648d9a9a0fad6c59692e3d5c0d6d655733e9
Maciej Sularz <sularz.maciej@gmail.com>
2016-10-24 02:39:18 +0200

    Added Intro component

--- src/js/pages/Index.js
+++ src/js/pages/Index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router";
 
 import Featured from "./index/Featured";
+import Intro from "./index/Intro";
 import CookieConsent from "./index/CookieConsent";
 
 // by exporting this class we can require it in client.js
@@ -176,19 +177,7 @@ export default class Index extends React.Component {
     </div>
 </div>
 </div>
-<div id="App_homepage_intro_containter">
-    <div id="App_homepage_intro_content">
-        <h3>
-            We believe that data acquisition, processing and
-            management become an ultimate source of competitive
-            advantage. We want to be with you at the forefront
-            of this revolution.
-        </h3>
-        <Link to="company/overview" id="App_homepage_intro_link">
-            Get to know more about Data Invest &gt;
-        </Link>
-    </div>
-</div>
+<Intro />
 <Featured />
 <CookieConsent />
 </div>
